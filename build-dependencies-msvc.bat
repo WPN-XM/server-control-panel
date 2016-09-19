2a4b1367170249afdfea1b1362e13b57f6214c4f
jakoch <jakoch@web.de>
2016-09-19 17:30:14 +0200

    use CMAKE_GENERATOR

--- build-dependencies-msvc.bat
+++ build-dependencies-msvc.bat
@@ -4,16 +4,6 @@ echo.
 echo "Building dependencies using MSVC"
 echo.
 
-:: configure QT
-pushd "%~dp0"
-call "%QTDIR%\bin\qtenv2.bat"
-popd
-
-:: configure MSVC
-SET VSVER=14.0
-pushd "%~dp0"
-call "%ProgramFiles(x86)%\Microsoft Visual Studio %VSVER%\VC\vcvarsall.bat"
-popd
 
 
 echo -- Building zlib
@@ -21,7 +11,7 @@ echo.
 pushd "%~dp0"
 cd third-party\zlib
 :: 1) configure
-cmake -G"Visual Studio 14 2015" CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
+cmake -G"%CMAKE_GENERATOR%" CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
 :: 2) make
 msbuild .\build\zlib.vcxproj /p:Configuration=Release /verbosity:minimal /p:OutDir=..\..\..\libs\
 msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /verbosity:minimal /p:OutDir=..\..\..\libs\
@@ -35,8 +25,9 @@ pushd "%~dp0"
 cd third-party\quazip\quazip
 :: 1) configure
 mkdir build && cd build
-cmake.exe .. -DCMAKE_INSTALL_PREFIX=../../../libs -DCONFIG+=statliclib -DINCLUDEPATH="$$PWD/../../zlib" -DLIBS+="-L$$PWD/../../../libs -lz" -DDESTDIR="$$PWD/../../../libs"
+cmake .. -G"%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX=../../../libs -DCONFIG+=statliclib -DINCLUDEPATH="$$PWD/../../zlib" -DLIBS+="-L$$PWD/../../../libs -lz" -DDESTDIR="$$PWD/../../../libs"
 :: 2) make
+cmake --build . --config %CONFIGURATION%
 popd
 echo Done.
 echo.
