ff8e6213864f24cf3e9a13c8199aee173606abd9
jakoch <jakoch@web.de>
2016-09-23 13:13:53 +0200

    even more trial and error commits...

--- build-dependencies-msvc.bat
+++ build-dependencies-msvc.bat
@@ -11,8 +11,8 @@ cd third-party\zlib
 :: 1) configure
 cmake -G "Visual Studio 14 2015" CMakeLists.txt -B./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../libs
 :: 2) make
-msbuild .\build\zlib.vcxproj /p:Configuration=Release /verbosity:minimal /p:OutDir=..\..\..\libs\
-msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /verbosity:minimal /p:OutDir=..\..\..\libs\
+msbuild .\build\zlib.vcxproj /p:Configuration=Release /p:OutDir=..\..\..\libs\ /nologo /verbosity:minimal /maxcpucount
+msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /p:OutDir=..\..\..\libs\ /nologo /verbosity:minimal /maxcpucount
 popd
 echo Done.
 echo.
@@ -21,11 +21,10 @@ echo -- Building QuaZip
 echo.
 pushd "%~dp0"
 cd third-party\quazip\quazip
-call qtenv2.bat
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 :: 1) configure
-mkdir build && cd build
-cmake .. -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../../libs -DCONFIG+=statliclib -DINCLUDEPATH="$$PWD/../../zlib" -DLIBS+="-L$$PWD/../../../libs -lz" -DDESTDIR="$$PWD/../../../libs"
+mkdir build 
+cd build
+cmake .. -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../../libs -DCONFIG+=statliclib -DCMAKE_PREFIX_PATH="C:\Qt\%qt%" -DINCLUDEPATH="$$PWD/../../zlib" -DLIBS+="-L$$PWD/../../../libs -lz" -DDESTDIR="$$PWD/../../../libs"
 :: 2) make
 cmake --build . --config %CONFIGURATION%
 popd
