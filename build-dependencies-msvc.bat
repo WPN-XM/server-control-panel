4ad5c835b1a118e82325dc78dd666f9266b8593c
jakoch <jakoch@web.de>
2018-01-04 03:59:11 +0100

    parameter cleanup build yaml-cpp

--- build-dependencies-msvc.bat
+++ build-dependencies-msvc.bat
@@ -2,17 +2,35 @@
 
 echo.
 echo Building dependencies using MSVC
+echo. 
+echo Using CMake Generator: Visual Studio 14 2015 Win64 =  (x86_amd64)
+echo.
+
+echo -- Set Qt folder
+echo.
+SET "QTVERSION=5.9.2\msvc2017_64"
+SET QTDIR=C:\Qt\%QTVERSION%
+rem SET PATH=%QTDIR%\bin;%QTDIR%;%PATH%
+echo %QTDIR%\bin
+call %QTDIR%\bin\qtenv2.bat
+
+SET "platform=amd64"
+SET "MSBUILD_FLAGS=/verbosity:minimal /maxcpucount /nologo"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %platform%
+echo Done.
 echo.
 
 echo -- Building zlib
 echo.
 pushd "%~dp0"
 cd third-party\zlib
+:: 0) cleanup
+rm -rf build
 :: 1) configure
-cmake -G "Visual Studio 14 2015" CMakeLists.txt -B./build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../libs
+cmake -G "Visual Studio 14 2015 Win64" CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
 :: 2) make
-msbuild .\build\zlib.vcxproj /p:Configuration=Release /p:OutDir=..\..\..\libs\ /nologo /verbosity:minimal /maxcpucount
-msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /p:OutDir=..\..\..\libs\ /nologo /verbosity:minimal /maxcpucount
+msbuild .\build\zlib.vcxproj /p:Configuration=Release %MSBUILD_FLAGS%
+msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /p:Platform=%platform% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
 popd
 echo Done.
 echo.
@@ -20,14 +38,31 @@ echo.
 echo -- Building QuaZip
 echo.
 pushd "%~dp0"
-echo C:\Qt\%qt%\bin
 cd third-party\quazip\quazip
+:: 0) cleanup
+rm -rf build
 :: 1) configure
 mkdir build 
 cd build
-cmake .. -G "Visual Studio 14 2015" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../../libs -DQT_MOC_EXECUTABLE="C:\Qt\%qt%\bin" -DCMAKE_PREFIX_PATH="C:\Qt\%qt%\bin" -DCONFIG+=statliclib  -DINCLUDEPATH="$$PWD/../../zlib" -DLIBS+="-L$$PWD/../../../libs -lz" -DDESTDIR="$$PWD/../../../libs"
+cmake .. -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=../../../libs -DLIB_DESTINATION=../../../libs/quazip/bin -DZLIB_INCLUDE_DIRS=../../../libs/zlib/include -DZLIB_LIBRARIES=../../../libs/zdll.lib
 :: 2) make
-cmake --build . --config %CONFIGURATION%
+msbuild ..\quazip.vcxproj /p:Configuration=Release /p:Platform=%platform% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
+popd
+echo Done.
+echo.
+
+echo -- Building yaml-cpp
+echo.
+pushd "%~dp0"
+cd third-party\yaml-cpp
+:: 0) cleanup
+rm -rf build
+:: 1) configure
+mkdir build 
+cd build
+cmake .. -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=../../../libs -DBUILD_SHARED_LIBS=ON
+:: 2) make
+msbuild yaml-cpp.vcxproj /p:Configuration=Release /p:Platform=%platform% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
 popd
 echo Done.
 echo.
