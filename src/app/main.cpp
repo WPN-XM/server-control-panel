73c64a971050df4779e24af249b265ca67ea5c6e
jakoch <jakoch@web.de>
2018-06-07 11:25:17 +0200

    hopefully better High-Dpi support: switch from Qt::AA_Use96Dpi to Qt::AA_UseHighDpiPixmaps + Qt::AA_EnableHighDpiScaling issue https://github.com/WPN-XM/WPN-XM/issues/618

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -68,13 +68,9 @@ int main(int argc, char *argv[])
     // do not leave application, until Quit is clicked in the tray menu
     app.setQuitOnLastWindowClosed(false);
 
-    /**
-   * Assume the screen has a resolution of 96 DPI rather than using
-   * the OS-provided resolution. This will cause font rendering to
-   * be consistent in pixels-per-point across devices rather than
-   * defining 1 point as 1/72 inch.
-   */
-    app.setAttribute(Qt::AA_Use96Dpi, true);
+    // HighDpi support
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    app.setAttribute(Qt::AA_EnableHighDpiScaling);
 
     /**
    * @brief Set Style: "Fusion Dark"
