24f62b16f42ecda027e965a8022f87f8f3c8e182
jakoch <jakoch@web.de>
2019-01-26 17:49:05 +0100

    update submodules

--- third-party/update-submodules.bat
+++ third-party/update-submodules.bat
@@ -3,20 +3,23 @@
 git submodule status
 
 cd zlib
+git pull
 git checkout v1.2.11
 cd ..
 git add zlib
 git commit -m "updated submodule zlib to v1.2.11"
 
 cd yaml-cpp
+git pull
 rem git checkout yaml-cpp-0.6.2
-git checkout 4fb1c4b92bf8d94b32ebccdd890407d45b3bc794
+git checkout b87c76a2ef4f974e2739a2d410ee5e36122cfc00
 cd ..
 git add yaml-cpp
-rem git commit -m "updated submodule yaml-cpp to v0.5.2"
-git commit -m "updated submodule yaml-cpp to 4fb1c4"
+rem git commit -m "updated submodule yaml-cpp to v0.6.2"
+git commit -m "updated submodule yaml-cpp to b87c76a2"
 
 cd quazip 
+git pull
 git checkout 0.7.6
 cd ..
 git add quazip
