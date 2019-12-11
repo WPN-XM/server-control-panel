a1c97fb7ecefeb7f91d953a425d6a4c9356d422a
jakoch <jakoch@web.de>
2019-12-11 21:23:12 +0100

    updating to -G "Visual Studio 16 2019" -A x64

--- build-dependencies-msvc.bat
+++ build-dependencies-msvc.bat
@@ -4,15 +4,9 @@ echo.
 echo Building dependencies using MSVC
 echo. 
 
-echo -- Set Generator
-echo. 
-SET "GENERATOR=Visual Studio 14 2015 Win64"
-echo    Using CMake Generator: %GENERATOR%
-echo.
-
 echo -- Set Qt folder
 echo.
-SET "QTVERSION=Qt5.10.1\5.10.1\msvc2017_64"
+SET "QTVERSION=5.13.2\msvc2017_64"
 SET QTDIR=D:\Qt\%QTVERSION%
 SET PATH=%QTDIR%\bin;%QTDIR%;%PATH%
 echo    Using Qt: %QTDIR%\bin
@@ -26,7 +20,8 @@ echo.
 echo -- Setup MSVC environment
 SET "PLATFORM=amd64"
 SET "MSBUILD_FLAGS=/verbosity:minimal /maxcpucount /nologo"
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
+:: WHAT THE FUCK
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
 echo Done.
 echo.
 
@@ -37,7 +32,7 @@ cd third-party\zlib
 :: 0) cleanup
 rm -rf build
 :: 1) configure
-cmake -G %GENERATOR% CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
+cmake -G "Visual Studio 16 2019" -A x64 CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
 :: 2) make
 msbuild .\build\zlib.vcxproj /p:Configuration=Release %MSBUILD_FLAGS%
 msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
@@ -56,7 +51,7 @@ rm -rf build
 :: 1) configure
 mkdir build 
 cd build
-cmake .. -G %GENERATOR% -DCMAKE_INSTALL_PREFIX=../../../libs -DLIB_DESTINATION=../../../libs/quazip/bin -DZLIB_INCLUDE_DIRS=../../../libs/zlib/include -DZLIB_LIBRARIES=../../../libs/zdll.lib
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs -DLIB_DESTINATION=../../../libs/quazip/bin -DZLIB_INCLUDE_DIRS=../../../libs/zlib/include -DZLIB_LIBRARIES=../../../libs/zdll.lib
 :: 2) make
 msbuild ..\quazip.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
 :: 3) install 
@@ -74,7 +69,7 @@ rm -rf build
 :: 1) configure
 mkdir build 
 cd build
-cmake .. -G %GENERATOR% -DCMAKE_INSTALL_PREFIX=../../../libs -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF
 :: 2) make
 msbuild yaml-cpp.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs %MSBUILD_FLAGS%
 :: 3) install 
