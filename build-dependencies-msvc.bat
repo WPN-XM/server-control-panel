96787b28fd141af4d385e6eadb61a42be7ae32b1
jakoch <jakoch@web.de>
2019-12-12 23:31:27 +0100

    cleanup dependency building

--- build-dependencies-msvc.bat
+++ build-dependencies-msvc.bat
@@ -16,12 +16,8 @@ echo -- Setup Qt environment
 call %QTDIR%\bin\qtenv2.bat
 qmake -v
 echo.
-
 echo -- Setup MSVC environment
-SET "PLATFORM=amd64"
-SET "MSBUILD_FLAGS=/verbosity:minimal /maxcpucount /nologo"
-:: WHAT THE FUCK
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo Done.
 echo.
 
@@ -29,15 +25,13 @@ echo -- Building zlib
 echo.
 pushd "%~dp0"
 cd third-party\zlib
-:: 0) cleanup
 rm -rf build
+mkdir build 
+cd build
 :: 1) configure
-cmake -G "Visual Studio 16 2019" -A x64 CMakeLists.txt -B./build -DCMAKE_INSTALL_PREFIX=../../libs
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs/zlib
 :: 2) make
-msbuild .\build\zlib.vcxproj /p:Configuration=Release %MSBUILD_FLAGS%
-msbuild .\build\zlibstatic.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
-:: 3) install 
-msbuild .\build\INSTALL.vcxproj %MSBUILD_FLAGS% 
+cmake --build . --target install --config Release
 popd
 echo Done.
 echo.
@@ -46,16 +40,13 @@ echo -- Building QuaZip
 echo.
 pushd "%~dp0"
 cd third-party\quazip
-:: 0) cleanup
 rm -rf build
-:: 1) configure
 mkdir build 
 cd build
-cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs -DLIB_DESTINATION=../../../libs/quazip/bin -DZLIB_INCLUDE_DIRS=../../../libs/zlib/include -DZLIB_LIBRARIES=../../../libs/zdll.lib
+:: 1) configure
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs/quazip -DLIB_DESTINATION=../../../libs/quazip/bin -DZLIB_ROOT=../../../libs/zlib
 :: 2) make
-msbuild ..\quazip.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs\ %MSBUILD_FLAGS%
-:: 3) install 
-msbuild ..\INSTALL.vcxproj %MSBUILD_FLAGS% 
+cmake --build . --target install --config Release
 popd
 echo Done.
 echo.
@@ -64,16 +55,13 @@ echo -- Building yaml-cpp
 echo.
 pushd "%~dp0"
 cd third-party\yaml-cpp
-:: 0) cleanup
 rm -rf build
-:: 1) configure
 mkdir build 
 cd build
-cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF
+:: 1) configure
+cmake .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=../../../libs/yaml-cpp -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF
 :: 2) make
-msbuild yaml-cpp.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:OutDir=..\..\..\libs %MSBUILD_FLAGS%
-:: 3) install 
-msbuild INSTALL.vcxproj %MSBUILD_FLAGS% 
+cmake --build . --target install --config Release
 popd
 echo Done.
 echo.
