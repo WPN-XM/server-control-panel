c0b63405562bca02b8336726495cb4f0531cfba5
jakoch <jakoch@web.de>
2020-03-22 21:01:06 +0100

    added shortcuts for QStringLiteral, etc

--- src/core/common.h
+++ src/core/common.h
@@ -1,10 +1,30 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+//------------------------------------------------------------------------------
+// EXPORT Library
+//------------------------------------------------------------------------------
+
 #ifdef APP_CORE_SHAREDLIBRARY
 #define APP_CORE_EXPORT Q_DECL_EXPORT
 #else
 #define APP_CORE_EXPORT Q_DECL_IMPORT
+#endif
+
+//------------------------------------------------------------------------------
+// Shortcuts
+//------------------------------------------------------------------------------
+
+#ifndef QSL
+#define QSL(x) QStringLiteral(x)
+#endif
+
+#ifndef QL1S
+#define QL1S(x) QLatin1String(x)
+#endif
+
+#ifndef QL1C
+#define QL1C(x) QLatin1Char(x)
 #endif
 
 #endif // COMMON_H
