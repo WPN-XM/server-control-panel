8632398bfd7299c20e3274d5e40cafd5595f5bcf
jakoch <jakoch@web.de>
2020-03-27 12:33:02 +0100

    added WINDEPLOYQT_BUILD_TYPE_ARG to deploy release, debug libs

--- cmake/windeployqt.cmake
+++ cmake/windeployqt.cmake
@@ -1,12 +1,30 @@
 find_package(Qt5Core REQUIRED)
 
-# 1. get absolute path to qmake, then use it to find windeployqt executable
+
+# 1. set WINDEPLOYQT_BUILD_TYPE_ARG
+# this is passed as argument to windeployqt so that release/debug libs are deployed
+
+if( CMAKE_BUILD_TYPE STREQUAL "Debug" )
+  set(WINDEPLOYQT_BUILD_TYPE_ARG "--debug")
+elseif( CMAKE_BUILD_TYPE STREQUAL "Release")
+   set(WINDEPLOYQT_BUILD_TYPE_ARG "--release")
+elseif( CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+  set(WINDEPLOYQT_BUILD_TYPE_ARG "--release-with-debug-info")
+endif()
+
+# 2. get absolute path to qmake, then use it to find windeployqt executable
 
 get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
 find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
 
+# 3. windeploy qt function
+
 function(windeployqt target)
+
+    if(NOT target)
+        message(FATAL_ERROR "Please specify a build TARGET for windeployqt.")
+    endif()
 
     # POST_BUILD step
     # - after build, we have a bin/lib for analyzing qt dependencies
@@ -18,7 +36,7 @@ function(windeployqt target)
         COMMAND "${WINDEPLOYQT_EXECUTABLE}"
                 --dir "${CMAKE_CURRENT_BINARY_DIR}/windeployqt"          
                 --verbose 1
-                --release
+                ${WINDEPLOYQT_BUILD_TYPE_ARG}
                 --no-svg
                 --no-angle
                 --no-opengl
